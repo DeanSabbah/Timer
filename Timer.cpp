05e9d6dc65584961f668a4feba5aca4f90b5a0e5
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-04-19 05:33:57 -0400

    first commit

---
+++ Timer.cpp
@@ -0,0 +1,33 @@
+﻿// Timer.cpp : Defines the entry point for the application.
+//
+
+#include "Timer.h"
+
+Timer::Timer(){}
+Timer::~Timer() {}
+
+void Timer::start(float timer){
+	time(&start_time_);
+	time_ = timer;
+	running = true;
+	finished = false;
+}
+
+float Timer::check_time(){
+	time_t curr_time, time_diff;
+	time(&curr_time);
+	time_diff = curr_time - start_time_;
+	if (time_diff >= time_){
+		finished = true;
+		time_diff = time_;
+	}
+	return time_diff;
+}
+
+bool Timer::check_done(){
+	if(check_time() >= time_ && running){
+		running = false;
+		return true;
+	}
+	return false;
+}
---
+++ Timer.h
@@ -0,0 +1,40 @@
+﻿#ifndef TIMER_H
+#define TIMER_H
+
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <ctime>
+
+class Timer {
+	public:
+		Timer();
+		~Timer();
+		template<typename Lambda>
+		void start(float timer, Lambda&& lambda){
+			time(&start_time_);
+			time_ = timer;
+			running = true;
+			finished = false;
+			std::thread([=]() {
+				std::this_thread::sleep_for(std::chrono::milliseconds((int)(timer * 1000)));
+				lambda();
+				}).detach();
+		}
+		void start(float time);
+
+		float check_time();
+		bool check_done();
+	
+	private:
+		time_t start_time_ = NULL;
+		time_t time_ = NULL;
+
+		bool finished = false;
+		bool running = false;
+		
+};
+
+#endif // TIMER_H
