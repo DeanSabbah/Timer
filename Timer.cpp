11bb879798e7fd367afd95224f40241e33bf7f21
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-05-12 12:46:11 -0400

    Use functional instead of template. Moved function definition for start to cpp file.

--- Timer.cpp
+++ Timer.cpp
@@ -5,6 +5,8 @@ Timer::~Timer() {}
 
 /**
 * Starts the timer without a lambda function.
+* 
+* @param <float> time: The time in seconds to wait before executing the lambda function.
 */
 void Timer::start(float timer){
 	start_time_ = std::chrono::steady_clock::now();
@@ -13,10 +15,28 @@ void Timer::start(float timer){
 	finished = false;
 }
 
-/*
+/**
+* Starts the timer with a lambda function.
+* @param <float> time The time in seconds to wait before executing the lambda function.
+* @param <void()> lambda: The lambda function to execute after the timer expires.
+* 
+* If the passed function's parent object is deleted (dynamic) or goes out of scope (static), this
+* will result in a read access violation.
+*/
+void Timer::start(float time, std::function<void()> lambda) {
+	start(time);
+	std::thread([time, lambda]() mutable {
+		std::this_thread::sleep_for(std::chrono::milliseconds((int)(time * 1000)));
+		lambda();
+		}).detach();
+}
+
+/**
 * This function will check the time elapsed since the timer started.
 * If the time elapsed is greater than or equal to the timer set, it will
 * set finished to true and return the time elapsed.
+* 
+* @returns <float>: Time since timer started
 */
 float Timer::check_time() {
     if (!running) {
@@ -32,12 +52,15 @@ float Timer::check_time() {
     return time_diff;
 }
 
-/*
+/**
 * This function will check if the timer is done.
 * Only returns true if the timer was started since the last time it finished (or if it never finished).
+* (In other words, the timer must be running)
+* 
+* @returns <bool>: True if finished, false otherwise
 */
 bool Timer::check_done(){
-	if(check_time() >= time_ && running)
+	if(running && check_time() >= time_)
 		running = false;
 	
 	return finished;
--- Timer.h
+++ Timer.h
@@ -3,27 +3,14 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <functional>
 
 class Timer {
 	public:
 		Timer();
 		~Timer();
-		/**
-		 * Starts the timer with a lambda function.
-		 * @param timer The time in seconds to wait before executing the lambda function.
-		 * @param lambda The lambda function to execute after the timer expires.
-		 */
-		template<typename Lambda>
-		void start(float timer, Lambda&& lambda){
-			start_time_ = std::chrono::steady_clock::now();
-			time_ = timer;
-			running = true;
-			finished = false;
-			std::thread([timer, lambda = std::forward<Lambda>(lambda)]() mutable {
-				std::this_thread::sleep_for(std::chrono::milliseconds((int)(timer * 1000)));
-				lambda();
-				}).detach();
-		}
+		
+		void start(float time, std::function<void()> lambda);
 		void start(float time);
 
 		float check_time();
